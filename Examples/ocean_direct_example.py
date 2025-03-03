8e9501a108d8ac7322e5055ac1a1732d3f5df641
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-03-03 20:29:45 +0000

    Update variable naming for clarity
    Renamed `device_1` to `spectrometer_instance` for improved code readability.

--- Examples/ocean_direct_example.py
+++ Examples/ocean_direct_example.py
@@ -1,10 +1,10 @@
 from OceanDirect import OceanDirectAPI
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ocean_devices_manager = OceanDirectAPI()
     ids = ocean_devices_manager.get_device_ids()
     print(ids)
-    device_1 = ocean_devices_manager.open_device(ids[0])
+    spectrometer_instance = ocean_devices_manager.open_device(ids[0])
     print("Successfully opened device")
-    device_1.close_device()
+    spectrometer_instance.close_device()
     print("Successfully closed device")
